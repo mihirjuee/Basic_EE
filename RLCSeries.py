2ab27fcd420539adb57e809c4adecae4e7dd71af
mihirjuee <mihir.juee@gmail.com>
2026-04-17 17:01:13 +0530

    Update RLCSeries.py

--- RLCSeries.py
+++ RLCSeries.py
@@ -96,10 +96,10 @@ with col2:
     with tab1:
       fig_p = go.Figure()
       fig_p.add_trace(go.Scatter(x=[0, Vr], y=[0, 0], mode='lines+markers', name='Vr', line=dict(color='green', width=4)))
-     fig_p.add_trace(go.Scatter(x=[0, 0], y=[0, Vl], mode='lines+markers', name='Vl', line=dict(color='blue', width=4)))
+      fig_p.add_trace(go.Scatter(x=[0, 0], y=[0, Vl], mode='lines+markers', name='Vl', line=dict(color='blue', width=4)))
       fig_p.add_trace(go.Scatter(x=[0, 0], y=[0, -Vc], mode='lines+markers', name='Vc', line=dict(color='red', width=4)))
       fig_p.add_trace(go.Scatter(x=[0, I], y=[0, 0], mode='lines+markers', name='I', line=dict(color='orange', width=4)))
-     fig_p.add_trace(go.Scatter(x=[0, Vr], y=[0, X_net*I], mode='lines+markers', name='V Total', line=dict(color='black', width=3, dash='dash')))
+      fig_p.add_trace(go.Scatter(x=[0, Vr], y=[0, X_net*I], mode='lines+markers', name='V Total', line=dict(color='black', width=3, dash='dash')))
           
 limit = max(Vr, Vl, Vc) * 1.2
     fig_p.update_layout(xaxis=dict(range=[-limit/4, limit]), yaxis=dict(range=[-limit, limit]), height=450)
