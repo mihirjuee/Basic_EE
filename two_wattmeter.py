4845eb8ac8c8705b5709e0ad6cc94a4756d40671
mihirjuee <mihir.juee@gmail.com>
2026-04-23 16:29:23 +0530

    Update two_wattmeter.py

--- two_wattmeter.py
+++ two_wattmeter.py
@@ -34,7 +34,7 @@ def draw_textbook_circuit(ax):
     d += elm.Dot().label("R", 'left')
     d += (CC1 := elm.Inductor(loops=3).label("CC1", 'top'))
     d += elm.Line().right(1)
-     d += elm.Resistor().label("Zr").right()
+    d += elm.Resistor().label("Zr").right()
     d += (P1 := elm.Dot())
     # Connect PC1 to the node between CC and Load
     d += elm.Resistor().label("PC1", 'right').down(1.5)
